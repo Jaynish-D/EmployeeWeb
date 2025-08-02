f5ccd5887ca7d8ee8eb0664752636fc572401e7f
Jaynish Chauhan <19it191jd@gmail.com>
2025-08-02 17:22:55 +0530

    Employee add module structure.

--- EmployeeAPP/src/app/Components/employee-component/employee-component.ts
+++ EmployeeAPP/src/app/Components/employee-component/employee-component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-employee-component',
   imports: [],
   templateUrl: './employee-component.html',
-  styleUrl: './employee-component.css'
+  styleUrl: './employee-component.css',
 })
 export class EmployeeComponent {
+  @ViewChild('myModal') model: ElementRef | undefined;
 
+  openModel() {
+    const empModel = document.getElementById('myModal');
+    if (empModel != null) {
+      empModel.style.display = 'block';
+    }
+  }
+  closeModal() {
+    if (this.model != null) {
+      this.model.nativeElement.style.display = 'none';
+    }
+  }
 }
