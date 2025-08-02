4492f67c53393c8c6b3e70578b6b55951ca84a95
Jaynish Chauhan <19it191jd@gmail.com>
2025-08-02 18:34:28 +0530

    Add Employee FromGroup.

--- EmployeeAPP/src/app/Components/employee-component/employee-component.ts
+++ EmployeeAPP/src/app/Components/employee-component/employee-component.ts
@@ -1,13 +1,27 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-employee-component',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './employee-component.html',
   styleUrl: './employee-component.css',
 })
-export class EmployeeComponent {
+export class EmployeeComponent implements OnInit {
   @ViewChild('myModal') model: ElementRef | undefined;
+
+  employeeForm: FormGroup = new FormGroup({});
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.setFormState();
+  }
 
   openModel() {
     const empModel = document.getElementById('myModal');
@@ -19,5 +33,21 @@ export class EmployeeComponent {
     if (this.model != null) {
       this.model.nativeElement.style.display = 'none';
     }
+  }
+
+  setFormState() {
+    this.employeeForm = this.fb.group({
+      id: [0],
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required /* ,Validators.email */]],
+      mobile: ['', [Validators.required]],
+      age: ['', [Validators.required]],
+      salary: ['', [Validators.required]],
+      status: [false, [Validators.required]],
+    });
+  }
+
+  onSubmit() {
+    console.log(this.employeeForm.value);
   }
 }
